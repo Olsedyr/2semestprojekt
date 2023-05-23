08375831e198aaff8f1d97041af4a9315b32e3db
Patrick Dahl <patrick.v.d@hotmail.com>
2023-05-23 11:51:37 +0200

    Fully commented "ShopAccess"

--- src/main/java/CMS/Domain/ShopAccess.java
+++ src/main/java/CMS/Domain/ShopAccess.java
@@ -4,6 +4,7 @@ import CMS.ICMS;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,7 +24,7 @@ public class ShopAccess implements ICMS {
     //This function gets the HTML content of a file using the given id and template id.
     @Override
     public String getProductPage(String id, String template_id){
-        //This part gets the current information about the existing products and thumbnails and puts it into their
+        //This part gets the current information about the existing articles, products and thumbnails and puts it into their
         //respective HashMaps located in the class LoadingHashMaps.
 
         CMS.Domain.LoadingHashMaps.getInstance().textFilesIntoHashMaps();
@@ -45,7 +46,7 @@ public class ShopAccess implements ICMS {
     @Override
     public String getProductPage(String name, String description, double price, int stock, String id, File imageFile,
                           int template_id){
-        //This part gets the current information about the existing products and thumbnails and puts it into their
+        //This part gets the current information about the existing articles, products and thumbnails and puts it into their
         //respective HashMaps  located in the class LoadingHashMaps.
 
         CMS.Domain.LoadingHashMaps.getInstance().textFilesIntoHashMaps();
@@ -129,7 +130,7 @@ public class ShopAccess implements ICMS {
     //Read all article files into this HashMap and get a specific Entry based on its id.
     @Override
     public Map.Entry<String, String> getArticlePage(String title){
-        //This part gets the current information about the existing products and thumbnails and puts it into their
+        //This part gets the current information about the existing articles, products and thumbnails and puts it into their
         //respective HashMaps located in the class LoadingHashMaps.
 
         CMS.Domain.LoadingHashMaps.getInstance().textFilesIntoHashMaps();
@@ -159,7 +160,7 @@ public class ShopAccess implements ICMS {
 
         }
 
-        //This part returns the HTML content of entry with the given title as the entry's key.
+        //This part tries to return an entry with the given title as the entry's key.
 
         for (Map.Entry<String, String> entry : articles.entrySet()) {
             if (entry.getKey().equals(title)) {
@@ -175,12 +176,13 @@ public class ShopAccess implements ICMS {
     //Read all article files into this HashMap and get them all.
     @Override
     public HashMap<String, String> getArticlePages(){
-        //This part gets the current information about the existing products and thumbnails and puts it into their
+        //This part gets the current information about the existing articles, products and thumbnails and puts it into their
         //respective HashMaps located in the class LoadingHashMaps.
 
         CMS.Domain.LoadingHashMaps.getInstance().textFilesIntoHashMaps();
 
-        //This part iterates over every entry in the HashMap articles from the class LoadingHashMaps and converts the information in the entry's value into HTML
+        //This part iterates over every entry in the HashMap articles from the class LoadingHashMaps
+        // and converts the information in the entry's value into HTML
         //using the information from the entry's value.
 
         for(Map.Entry<String, String> entry : CMS.Domain.LoadingHashMaps.getInstance().getArticles().entrySet()) {
@@ -194,7 +196,8 @@ public class ShopAccess implements ICMS {
             //This part converts the information into HTML.
 
             try {
-                articleInHTML = Create.create(array[0].substring(0, array[0].indexOf("---")), array[1], array[2], array[3], Integer.parseInt(array[0].substring(array[0].indexOf("---") + 3)));
+                articleInHTML = Create.create(array[0].substring(0, array[0].indexOf("---")), array[1], array[2], array[3],
+                        Integer.parseInt(array[0].substring(array[0].indexOf("---") + 3)));
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -205,7 +208,7 @@ public class ShopAccess implements ICMS {
 
         }
 
-        //This part returns the HTML content of every existing article. 
+        //This part returns the HTML content of every existing article.
 
         return articles;
     }
@@ -213,38 +216,88 @@ public class ShopAccess implements ICMS {
     //Read all thumbnail files into this HashMap and get one based on its id.
     @Override
     public Map.Entry<String, String> getThumbnail(String title){
+        //This part gets the current information about the existing articles, products and thumbnails and puts it into their
+        //respective HashMaps located in the class LoadingHashMaps.
 
         CMS.Domain.LoadingHashMaps.getInstance().textFilesIntoHashMaps();
 
+        //This part iterates over every entry in the HashMap thumbnails from the class LoadingHashMaps,
+        //gets the HTML content from the file with the same id and template and saves it in this class' thumbnails HashMap
+        //with the same key.
+
         for (Map.Entry<String, String> entry : CMS.Domain.LoadingHashMaps.getInstance().getThumbnails().entrySet()) {
+
+            //This part reads the HTML content from any .txt file in the following folder containing the entry's key in its name.
+
+            Path htmlFilePath = Paths.get("src/main/data/Thumbnails/" + entry.getKey() + "_thumbnail.txt");
+
+            String htmlContent = null;
+            try {
+                htmlContent = Files.readString(htmlFilePath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            //This part puts the new information into the HashMap,
+            // overwriting any existing entry that has the same key with the new information.
+
+            if(thumbnails.containsKey(entry.getKey())){
+                thumbnails.replace(entry.getKey(), htmlContent);
+            } else {
+
+                thumbnails.put(entry.getKey(), htmlContent);
+            }
+        }
+
+        //This part tries to return an entry with the given title as the entry's key.
+
+        for (Map.Entry<String, String> entry : thumbnails.entrySet()) {
             if (entry.getKey().equals(title)) {
                 return entry;
             }
         }
-        return null; // Key not found in the HashMap
+
+        //If the key is not found in the HashMap, null is returned.
+
+        return null;
     }
 
     //Read all thumbnail files into this HashMap and get them all.
     @Override
     public HashMap<String, String> getThumbnails(){
 
+
+        //This part gets the current information about the existing articles, products and thumbnails and puts it into their
+        //respective HashMaps located in the class LoadingHashMaps.
+
         CMS.Domain.LoadingHashMaps.getInstance().textFilesIntoHashMaps();
 
-
+        //This part iterates over every entry in the HashMap thumbnails from the class LoadingHashMaps,
+        //gets the HTML content from the file with the same id and template and saves it in this class' thumbnails HashMap
+        //with the same key.
 
         for(Map.Entry<String, String> entry : CMS.Domain.LoadingHashMaps.getInstance().getThumbnails().entrySet()) {
-
+            String htmlContent;
             try {
-
                 Path htmlFilePath = Paths.get("src/main/data/Thumbnails/" + entry.getKey() + ".txt");
 
-                String htmlContent = Files.readString(htmlFilePath);
-
-                thumbnails.put(entry.getKey(), htmlContent);
+                htmlContent = Files.readString(htmlFilePath);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+
+            //This part puts the new information into the HashMap,
+            // overwriting any existing entry that has the same key with the new information.
+
+            if(thumbnails.containsKey(entry.getKey())){
+                thumbnails.replace(entry.getKey(), htmlContent);
+            } else {
+
+                thumbnails.put(entry.getKey(), htmlContent);
+            }
         }
+        //This part returns the HTML content of every existing thumbnail.
+
         return thumbnails;
     }
 
